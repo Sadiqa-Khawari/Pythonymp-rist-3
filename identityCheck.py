35f7ea18a01e7ea15497389b5ad3987f1a40aa68
Sadiqa-Khawari <53186@edu.raseko.fi>
2024-10-03 12:51:25 +0300

    Refaktorointi: korjattu päiväkistus

--- identityCheck.py
+++ identityCheck.py
@@ -25,3 +25,57 @@ def opeskelijanumeroOk(opeskelijanumero: str) -> bool:
     if pituus == 5 or pituus == 6:
             result = True
     return result
+
+# Henkilotunnus esimerkki 130787-676 testataan
+# 1. Pituus 
+# 2. Syntymäaikaosan oikeellisuus
+# 3. Vuosisatakoodit +, - ja A
+# 4. Moduli 31 tarkistus
+
+# Loullisena tavoitteena on funktio, joka tarkistaa henkilötunnuksen oikeellisuuden ja 
+
+def checkHeTu(hetu):
+  
+    # Oletustulos 0 ok jos kaikki on kunnossa
+    result = (0, "ok")
+
+    # Lasketaan HeTu-parametrin pitus
+    length = len(hetu)
+
+    # Jos pitus oikea tehdään eri osat
+    if length == 11:
+        dayPart = hetu[0:2]
+        monthPart = hetu[2:4]
+        yearPart = hetu[4:6]
+        centuryPart = hetu[6:7]
+        numberPart = hetu[7:10]
+        checkSum = hetu[10]
+
+        #  Tarkistwtaan päiväosan oikeellisuus pitää olla pelkkiä numeroita
+    if dayPart.isdigit():
+        day = int(dayPart)
+
+        # Päivän pitää olla väliltä 1 -31
+        if day < 1:
+            result = (3, "Päivä virheellinen")
+        if day > 31:
+            result = (3, "Päivä virheellinen")
+    else:
+            # Jos muuta kuin pelkkiä numeroita
+            result = (3, "Päivä virheellinen")
+       
+    if length < 11:
+        result = ( 1, "Henkilötunnus liian lyhyt")
+    
+    if length > 11:
+        result = ( 2, "Henkilötunnus liian pitkä")
+
+   
+    return result
+
+if __name__ == "__main__":
+     hetu = "130728-478N"
+     paivat = hetu[0:2]
+     kuukaudet = hetu[2:4]
+     print(paivat)
+     print(kuukaudet)
--- test_identityCheck.py
+++ test_identityCheck.py
@@ -17,3 +17,51 @@ def test_opeskelijanumeroOk_7():
 def test_opeskelijanumeroOk_5():
     assert identityCheck.opeskelijanumeroOk("12345") == False
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test_checkHeTuOk():
+    assert identityCheck.checkHeTu("130728-478N") == (0, "OK")
+
+def test_checkHeTuShort():
+    assert identityCheck.checkHeTu("13028-478N") == (1, "Henkilötunnus liian lyhyt")
+
+# Henkilötunnuksessa pitää olla 11 merkiä, merkijä on liika
+def test_checkHeTuLong():
+    assert identityCheck.checkHeTu("1307288-478N") == (2, "Henkilötunnus liian piykä")
+
+# Henkilötunnuksen päiväosassa saa olla 01 - 31
+def test_checkHeTuDays():
+    assert identityCheck.checkHeTu("450728-478N") == (3, "Päivä viheellinen")
+
+# Henkilötunnuksenkuukausiosassa saa olla 01 - 12
+def test_checkHeTuMonths():
+    assert identityCheck.checkHeTu("132728-478N") == (4, "Kuukausi virheellinen")
+
+# Henkilötunnuksen vuosiossa saa olla 00 -99
+def test_checkHeTuYears():
+    assert identityCheck.checkHeTu("13072x-478N") == (5, "Vuosi virheellinen")
+
+# Käytössaä olevat vuosisakoodit + (1800), - (1900) ja (2000)
+def test_checkHeTuCenturyCode():
+    assert identityCheck.checkHeTu("130728s478N") == (5, "Vuosisatakoodi virheellinen")
+
+# Henkilötunnuksen numeroista tehdään luku. esim 130728478 ja jaetaan se luvulla
+# 31. Jkojäännös on tarkiste. Jos se on alle 10, käytetään numeroa, jos yli haetaan taulukosta vastaava kirainmrkki
+# 10 -> A, 11 -> B. G ja I eivät ole käyt
+def test_checkHeTuModulo():
+    assert identityCheck.checkHeTu("130728-478M")

1cba0c792c982efc95c271d8bcf77cdd2c7a6dee
Sadiqa-Khawari <53186@edu.raseko.fi>
2024-10-24 13:29:03 +0300

    Lissätty tehtäviä asioita TODO

--- app.py
+++ app.py
@@ -8,17 +8,26 @@
 # --------
 
 #from avtools import sound # Äänimerkit ja äänitiedostot
-# from avtools import video # Videomoduuli
+#from avtools import video # Videomoduuli
 import identityCheck2
 
 # ASETUKSET
 # ---------
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
+# TODO: Pääohjelma ikuinen silmukka, josta poistutaan tarvittaessa (kaksi mekanismi itse)
+# TODO: Paranna pääohjelmaa siten, että se ei kaadu, kun käytää virheelisen henkilötunnuksen
 userGivenSsn =  input("Syötä asiakkaan henkilötunnus: ")
+userGivenLastname = input("Syötä asiakkan sukunimi")
+# TODO: Tee tarkistus siitä, että nim ei voi olla tyhjä
+userGivenFirstname = input("Syötä asikkan etunimi")
+# TODO: Tee tarkistus siitä, että nim ei voi olla tyhjä
+# TODO: Varautu tilanteeseen, jossa hetu:n tarkista on annettu pienillä kirjaimilla
+# TODO: Muuta syötettyjen nimien alkukirjan
+
 ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
 if ssnToCheck.isValidSsn() == True:
-    dateOfBirth = ssnToCheck.getDateOfBirth()
+    ssnToCheck.getDateOfBirth()
     ssnToCheck.getGender()
     age = ssnToCheck.calculateAge()
     print("Syntymäaika:", ssnToCheck.dateOfBirth)

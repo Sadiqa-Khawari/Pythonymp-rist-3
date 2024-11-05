2cf62de3d9038c4a53c30a59e6337d36369cb468
Sadiqa-Khawari <53186@edu.raseko.fi>
2024-11-05 09:33:05 +0200

    Aloitettu luokan lunti

--- barcode.py
+++ barcode.py
@@ -1,40 +1,40 @@
-# MODUFLI vvVACOODIEN TUOTTAMISEEN 
-# ================================
+# MODUULI VIIVAKOODIEN TUOTTAMISEEN
+# =================================
 
-# kIRJASTOT
+# KIRJASTOT
 # ---------
 
 # ASETUKSET
 # ---------
 
-# FUNKITOT
+# FUNKTIOT
 # --------
 
 def barCodeValue(character: str) -> int:
     """Calculates a value of character used in Code128B barcode generation
 
     Args:
-        character (str): a single chracter to convert
+        character (str): a single character to convert
 
     Returns:
         int: Code128B value for calcultaing the checksum
     """
     asciiValue = ord(character)
-    code128BValue = asciiValue -32
+    code128BValue = asciiValue - 32
     return code128BValue
 
 def calculateCode128BCheksum(text: str) -> int:
     """Calculates a checksum for a given string
 
     Args:
-        text (str): texr string to use in a barcode
+        text (str): text string to use in a barcode
 
     Returns:
-        int: Modulo 103 checksum of weighd values
+        int: Modulo 103 checksum of weighted values
     """
     text = text.strip()
     numberOfLetters = len(text)
-    code128BChecksum = 0
+    weightedSum = 0
     for number in range(numberOfLetters):
         letter = text[number]
         code128BValue = barCodeValue(letter)
@@ -44,9 +44,9 @@ def calculateCode128BCheksum(text: str) -> int:
     code128BChecksum = weightedSum % 103
     return code128BChecksum
 
-#TODO :Tee tämä funktio loppuun ja te
+
 def createCode128B(text: str) -> str:
-    """Creates a complete code 128B to be printed using libre code 128 font
+    """Creates a complete code128B barcode to be printed using Libre Code128 font
 
     Args:
         text (str): The text for a barcode without checksum
@@ -54,10 +54,14 @@ def createCode128B(text: str) -> str:
     Returns:
         str: String containing start, barcode, checksum and stop symbols
     """
-
-    code128BarcodeString = ""
+    code128BarcodeString = ''
+    startChar = chr(204)
+    stopChar = chr(206)
+    checkSum = calculateCode128BCheksum(text)
+    checkSumSymbol = chr(checkSum + 32)
+    code128BarcodeString = startChar + text + checkSumSymbol + stopChar
     return code128BarcodeString
 
 if __name__ == "__main__":
-    testString = "128B"
-    print("painotetut arvot yhteensa:", calculateCode128BCheksum(testString))
+    testString = '128B'
+    print('painotetut arvot yhteensä:',calculateCode128BCheksum(testString))
